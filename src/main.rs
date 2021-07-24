9fc8ce2e64fddb112cb215cc83d08a0b540a5c20
vwood <vwood@vwood.org>
2021-07-25 00:31:43 +1200

    Add better CLI options

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+#[macro_use]
+extern crate clap;
+
 extern crate image;
 
 use image::ImageBuffer;
 use rayon::prelude::*;
 use serde_json::Value;
-use std::env;
 use std::error::Error;
 use std::fs;
+use std::time::SystemTime;
 
 mod raymarch;
 mod scene;
@@ -19,38 +22,7 @@ fn process_file(input_filename: &str) -> Result<Value, Box<dyn Error>> {
     Ok(v)
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let mut input_filename = "default.json";
-    let mut output_filename = "default.png";
-
-    if args.len() > 1 {
-        input_filename = &args[1];
-        if args.len() > 2 {
-            output_filename = &args[2];
-        }
-    }
-
-    println!("Input: {}", input_filename);
-
-    let result = process_file(input_filename);
-
-    let value = match result {
-        Ok(v) => v,
-        Err(error) => {
-            println!("Error: {}", error);
-            return;
-        }
-    };
-
-    let sdf = value["scene"].as_str().unwrap_or("");
-
-    println!("Ouput: {}", output_filename);
-
-    let width = 800;
-    let height = 600;
-
+fn parallel_march(width: u32, height: u32, scene: &str) -> ImageBuffer<image::Rgb<u8>, Vec<u8>> {
     let pixels: Vec<u8> = (0..width * height)
         .into_par_iter()
         .map(|i| {
@@ -63,7 +35,7 @@ fn main() {
             let dir = vector::Vec3::new(-0.2 - y_pos, x_pos, 1.0).normalize();
 
             let (r, g, b) = raymarch::march(
-                match sdf {
+                match scene {
                     "torus" => &scene::torus_scene_sdf,
                     "mandlebulb" => &scene::mandlebulb_scene_sdf,
                     "gyroid" => &scene::gyroid_scene_sdf,
@@ -85,43 +57,105 @@ fn main() {
         .flatten()
         .collect();
 
-    println!("vec_length: {}", width * height * 3);
-    println!("vec_length: {}", pixels.len());
+    assert_eq!(width * height * 3, pixels.len() as u32);
 
-    let img = ImageBuffer::<image::Rgb<u8>, _>::from_vec(width, height, pixels).unwrap();
+    ImageBuffer::<image::Rgb<u8>, _>::from_vec(width, height, pixels).unwrap()
+}
 
-    /*
-        let mut img = ImageBuffer::new(width, height);
+#[allow(dead_code)]
+fn march(width: u32, height: u32, scene: &str) -> ImageBuffer<image::Rgb<u8>, Vec<u8>> {
+    let mut img = ImageBuffer::new(width, height);
 
-        for (x, y, pixel) in img.enumerate_pixels_mut() {
-            // generate primary ray
+    for (x, y, pixel) in img.enumerate_pixels_mut() {
+        // generate primary ray
 
-            let x_pos = ((x as f32) / (width as f32) - 0.5) * 0.8;
-            let y_pos = (0.5 - (y as f32) / (height as f32)) * 0.6;
+        let x_pos = ((x as f32) / (width as f32) - 0.5) * 0.8;
+        let y_pos = (0.5 - (y as f32) / (height as f32)) * 0.6;
 
-            let dir = vector::Vec3::new(-0.2 - y_pos, x_pos, 1.0).normalize();
+        let dir = vector::Vec3::new(-0.2 - y_pos, x_pos, 1.0).normalize();
 
-            let (r, g, b) = raymarch::march(
-                match sdf {
-                    "torus" => &scene::torus_scene_sdf,
-                    "mandlebulb" => &scene::mandlebulb_scene_sdf,
-                    "gyroid" => &scene::gyroid_scene_sdf,
-                    "example" => &scene::example_scene_sdf,
-                    _ => &scene::example_scene_sdf,
-                },
-                vector::Vec3::new(0.5, 0.5, -2.0),
-                dir,
-                100,
-                255.0,
-                0.001,
-            );
+        let (r, g, b) = raymarch::march(
+            match scene {
+                "torus" => &scene::torus_scene_sdf,
+                "mandlebulb" => &scene::mandlebulb_scene_sdf,
+                "gyroid" => &scene::gyroid_scene_sdf,
+                "example" => &scene::example_scene_sdf,
+                _ => &scene::example_scene_sdf,
+            },
+            vector::Vec3::new(0.5, 0.5, -2.0),
+            dir,
+            100,
+            255.0,
+            0.001,
+        );
 
-            let r = (255.0 * r) as u8;
-            let g = (255.0 * g) as u8;
-            let b = (255.0 * b) as u8;
-            *pixel = image::Rgb([r, g, b]);
+        let r = (255.0 * r) as u8;
+        let g = (255.0 * g) as u8;
+        let b = (255.0 * b) as u8;
+        *pixel = image::Rgb([r, g, b]);
+    }
+
+    img
+}
+
+fn main() {
+    let matches = clap_app!(rustic =>
+        (version: "1.0")
+        (author: "V Wood <vwood@vwood.org>")
+        (about: "Simple Raymarcher")
+        (@arg INPUT: +required "Input file to use")
+        (@arg OUTPUT: +required "Name of output file")
+        (@arg threads: -t [count] "Sets the number of threads to use")
+    )
+    .get_matches();
+
+    let input_filename = matches.value_of("INPUT").unwrap_or("default.json");
+    let output_filename = matches.value_of("OUTPUT").unwrap_or("default.png");
+
+    let thread_count = matches
+        .value_of("threads")
+        .unwrap_or("")
+        .parse::<usize>()
+        .unwrap_or(4);
+
+    let result = process_file(input_filename);
+
+    let value = match result {
+        Ok(v) => v,
+        Err(error) => {
+            println!("Error: {}", error);
+            return;
         }
-    */
+    };
+
+    let sdf = value["scene"].as_str().unwrap_or("");
+
+    let width = 800;
+    let height = 600;
+
+    let start = SystemTime::now();
+    let img;
+    if thread_count > 1 {
+        // Set number of threads
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(thread_count)
+            .build_global()
+            .unwrap();
+        println!("Using {} threads", thread_count);
+
+        img = parallel_march(width, height, sdf);
+    } else {
+        println!("Threads disabled");
+        img = march(width, height, sdf);
+    }
+    let end = SystemTime::now();
+
+    let time_taken = end
+        .duration_since(start)
+        .expect("Negative time")
+        .as_millis();
+    println!("Took {}ms", time_taken);
 
     img.save(output_filename).unwrap();
+    println!("Wrote {}", output_filename);
 }
